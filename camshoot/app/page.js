8b9b50c51f2f0da602431e654ce0c0e0aa68b9c3
Mesup Adhikari <mesupadhikari@gmail.com>
2025-01-12 09:36:07 +0545

    Login Page Setup

--- camshoot/app/page.js
+++ camshoot/app/page.js
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-   <div>
-    <h2>Subscribe to Mesup</h2>
-   </div>
+    <div>
+      <h2>Subscribe to Tube Guruji</h2>
+      <Button variant="destructive">Subscribe</Button>
+      <UserButton/>
+    </div>
   );
 }
